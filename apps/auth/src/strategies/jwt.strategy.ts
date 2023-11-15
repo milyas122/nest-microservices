1c89333daf9c11d63c7237b86b5556581d8ff382
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-11-15 19:00:04 +0500

    bug fix - handle no token provided issue

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
@@ -14,14 +14,18 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: any) =>
-          request?.cookies?.Authentication || request?.Authentication,
-        ,
+          request?.cookies?.Authentication ||
+          request?.Authentication ||
+          request?.headers.Authentication,
       ]),
       secretOrKey: configService.get('JWT_SECRET'),
     });
   }
 
   async validate({ userId }: IPayloadToken) {
+    if (!userId) {
+      throw new UnauthorizedException();
+    }
     return await this.userService.getUser({ _id: userId });
   }
 }
